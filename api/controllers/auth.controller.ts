68d60b5899e1a906c3171e179c986b7268788ab1
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-01-25 13:13:52 +0100

    added git ignore

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -4,17 +4,3 @@ export const register = async (
 	res: Response,
 	next: NextFunction,
 ) => {};
-// export const register = async (req, res, next) => {
-// 	try {
-// 		const hash = bcrypt.hashSync(req.body.password, 5);
-// 		const newUser = new User({
-// 			...req.body,
-// 			password: hash,
-// 		});
-
-// 		await newUser.save();
-// 		res.status(201).send("User has been created");
-// 	} catch (err) {
-// 		next(err);
-// 	}
-// };
