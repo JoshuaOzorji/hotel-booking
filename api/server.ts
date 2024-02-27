b07d82cf8bcc5d6ff947146866870ba6618b54f8
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-02-27 04:51:36 +0100

    cors

--- api/server.ts
+++ api/server.ts
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || "https://reserva-booking.netlify.app/",
+		origin: process.env.FRONTEND_URL || "http://localhost:5173",
 		credentials: true,
 	}),
 );
