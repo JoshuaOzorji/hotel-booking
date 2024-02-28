9d06fdfbf9f2de5f1ad9003056993b8b6c685853
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-02-28 08:16:47 +0100

    api link removal for backend

--- api/server.ts
+++ api/server.ts
@@ -29,10 +29,17 @@ app.use(
 	}),
 );
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/my-hotels", myHotelRoutes);
-app.use("/api/hotels", hotelRoutes);
+// PRODUCTION
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/my-hotels", myHotelRoutes);
+app.use("/hotels", hotelRoutes);
+
+// DEVELOPMENT
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/my-hotels", myHotelRoutes);
+// app.use("/api/hotels", hotelRoutes);
 
 app.listen(7000, () => {
 	console.log("Server is running on port 7000");
